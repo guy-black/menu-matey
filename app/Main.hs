10fd9b65f410d8029256bb7e46848c46a9d8a136
guy <guyb2602@gmail.com>
2022-11-29 11:01:19 -0600

    uhhh

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main (main) where
 
-import Lib
+import Data.Text
+import Monomer
+
+data AppModel = AppModel
+    { fileLoaded :: Bool
+    } deriving (Eq, Show)
+
+data AppEvent = AppInit
+              deriving (Eq, Show)
 
 main :: IO ()
 main = someFunc
