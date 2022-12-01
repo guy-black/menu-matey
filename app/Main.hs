a293e6eddf87b7ec20d03bb1314049523ec7d881
guy <guyb2602@gmail.com>
2022-12-01 12:25:43 -0600

    successfully launched a graphical window that showed text

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,44 @@
 
 module Main (main) where
 
-import Data.Text
+import qualified Data.Text as T
 import Monomer
+import System.Environment
+    ( getArgs
+    )
+
 
 data AppModel = AppModel
-    { fileLoaded :: Bool
+    { ucfmlfile :: T.Text
     } deriving (Eq, Show)
 
 data AppEvent = AppInit
               deriving (Eq, Show)
 
+handleEvent
+  :: WidgetEnv AppModel AppEvent
+  -> WidgetNode AppModel AppEvent
+  -> AppModel
+  -> AppEvent
+  -> [AppEventResponse AppModel AppEvent]
+handleEvent wenv node model evt = case evt of
+  AppInit -> []
+
+buildUI
+  :: WidgetEnv AppModel AppEvent
+  -> AppModel
+  -> WidgetNode AppModel AppEvent
+buildUI wenv model = label "wello horld"
+
+
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  let model = AppModel (T.pack (unlines args)) in -- TODO: only set to true if the first argument is a file that can be pased as a UCFMLModel
+    startApp model handleEvent buildUI config where
+      config =
+        [ appWindowTitle "menumate"
+        , appInitEvent AppInit
+        , appTheme darkTheme
+        , appFontDef "Regular" "./assets/fonts/Roboto-Regular.ttf"
+        ]
