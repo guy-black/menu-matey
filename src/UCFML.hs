e6f4b3f37e2a5da7eac252f876de8b954cc2d855
guy <guyb2602@gmail.com>
2022-12-29 18:58:41 -0600

    guess who's back (back back) back on meds (meds meds) my head works (works works) works again (gain gain) guesswhosbackguesswhosbackguesswhosbackguesswhosback

--- src/UCFML.hs
+++ src/UCFML.hs
@@ -29,14 +29,16 @@ import qualified Data.Text as T
 -- types for representing UCFML
 -- ----------------------------
 
-data UCFMLFile = NoFile
-               | NotAFile
-               | RawFile T.Text
-               | ParseError T.Text
-               | ParsedFile UCFMLModel
+-- data type to represent whether or not a file was passed as an argument, if that
+-- file can be read, and if that file can be parsed into a UCFMLModel
+data UCFMLFile = NoFile -- no file was passed as an argument
+               | NotAFile --  cannot find/read the file passed as an argument
+               | RawFile T.Text -- file was read and is in raw text
+               | ParseError T.Text -- file couldn't be parsed because <reason>
+               | ParsedFile UCFMLModel -- file parsed succesfully
                deriving (Eq, Show)
 
-data UCFMLModel = UCFMLMdel
+data UCFMLModel = UCFMLModel
     { meta     :: UCFMLMeta
     , body     :: UCFMLBody
     , template :: UCFMLTemplate
@@ -65,7 +67,8 @@ data OptSect = OptSect
 data Option = Option
     { optitle :: UCFMLText
     , aboutop :: UCFMLText
-    , refvar  :: UCFMLText -- ?????? maybe not the best way????? maybe a special different type??? idk????
+    , refvar  :: UCFMLText -- needs to be UCFMLText because the refVar of an option needs
+                           -- to be able to be set programmatically by forEach
     , input   :: Input
     } deriving (Eq, Show)
 
@@ -125,6 +128,9 @@ data ListIn = ListIn
 data UCFMLVal = UBool UCFMLBool
               | UNum  UCFMLNum
               | UText UCFMLText
+              | UGate UCFMLGate
+              | UOp   UCFMLOperator
+              | UComp UCFMLComparison
               deriving (Eq, Show)
 
 data CompIn = CompIn [(UCFMLText, Input )] -- [ (label, input) ]
@@ -242,170 +248,62 @@ data UCFMLConFile = UCFMLConFile
     , lines    :: [ UCFMLText ]
     } deriving (Eq, Show)
 
-
 data UCFMLFilePath = UnixStyle UCFMLText
                    | WinStyle UCFMLText
                    deriving (Eq, Show)
 
--- ------------------------------
--- Functions for parsing raw Text
--- ------------------------------
-
--- new plan
--- -- no quoted text in a ucfml model should be parsed as a symbol of any sort
--- -- dancing around quoted bits parsing the comments is hard
--- -- dequote whole file first before processing
-
--- Dequoting funcitions and datatypes
--- -----------------------------------
-
-data Dequoted = Dequoted
-    { qtMap :: Map QtToken Text
-    , notQt :: [PossQuot]
-    }
-
-data QtToken = QtToken Int
-
--- # of \n in quotate   #of chars after last newline
-data QtLns = QtLns Int Int
-
-data PossQhot = Quo QtToken QtLns
-              | NoQ Text
-
-noqMap :: (T.Text -> a) -> [PossQuot] -> [a]
-noqMap _ [] = []
-noqMap f (p:ps) =
-  case p of
-    NoQ t -> (f p):(noqMap f ps)
-    Quo _ -> (noqMap f ps)
-
-dequote :: Dequoted -> T.Text -> Maybe Dequoted
-dequote (Dequote qto nqt) rt =
-  -- look for first "
-  let (bef,rst) = T.break ('"'==) rt in
-
-  -- if rst is empty, then no more quote symbols. there is nothing left to check
-  if (rst == "") then
-    -- reverse nqt before returning the value so everything looks right
-    Just Dequoted qto reverse$((NoQ bef):nqt)
-
-  -- check if the " comes on the same line as and after a line comment "--"
-  else if ("--" `T.isInfixOf`(head . reverse . T.lines) $ bef) then
-    -- if yes then this is a commented quote, recurse with all of bef AND the rest of the line in rest
-    -- added to notQt in acc, and the rest of rst after the linebreak
-    let (ln,txt) = T.break (=='\n') rst in
-      dequote (Dequote qto ((NoQ (bef<>ln)):nqt)) txt
-
-  -- check if its inside a block comment or just hanging loose or a full quot
-  else
-    case ((length . fst . breakOn "\"")T.tail $ rst)`ordering`((length . fst . breakOn "-}")T.tail $ rst) of
-      EQ -> Nothing -- unclosed quotation mark, return nothing for parse error
-      GT -> -- in a block comment, add everything up to the block closing bloc
-
-      LT -> -- is quoted text, add to map and recurse with (snd . breakOn "\"")T.tail $ rst
+-- -----------------
+-- parsing funcitons
+-- -----------------
 
 
--- remove all user comments --
--- ---------------------------
--- remove any comments from the file before messing further with it
--- remove any lines where the first nonwhitespace charactes are "--"
--- check all lines for an unquotted "--"
--- remove any text wrapped in unquoted "{-" and  "-}" and the brackets too
-{- -- *********** START OF BIG COMMENT COMMENT BLOCK **********
-uncomment :: T.Text -> T.Text
-uncomment = (rmBlkCmt . rmPrLnCmt . rmWhLnCmt)
+-- empty model to fill in as we go
+emptyModel :: UCFMLModel
+emptyModel = UCFMLModel emptyMeta emptyBody emptyTemp
 
--- remove whole line comments
-rmWhLnCmt :: T.Text -> T.Text
-rmWhLnCmt rt = T.unlines $ filter (\x->(not . T.isPrefixOf "--")(T.stripStart x)) $ T.lines rt
+emptyMeta :: UCFMLMeta
+emptyMeta = UCFMLMeta UnsetT UnsetT UnsetT UnsetT
 
--- remove partial line comments
-rmPrLnCmt :: T.Text -> T.Text
-rmPrLnCmt rt = T.unlines (rmPrLnCmt' [] (T.lines rt))
+emptyBody :: UCFMLBody
+emptyBody  UCFMLBody []
 
-rmPrLnCmt' :: [T.Text] -> [T.Text] -> [T.Text]
-rmPrLnCmt' acc [] = reverse acc
-rmPrLnCmt' acc (t:ts) =
-  case ((T.isInfixOf "--" t),(T.elem '"' t)) of
-    -- TODO both quotation mark(s) and line comment are in the line
-    (True, True) ->
-      case (ordering (T.length ((fst . T.breakOn "--")t))(T.length ((fst . T.breakOn "\"")t))) of
-        LT -> rmPrLnCmt' ((T.breakOn "--" t):acc) ts -- comment was first, treat as if there was only a comment
-        GT -> -- quote was first
-          -- TODO: change this to only cound non escaped quote
-          -- --  in "this is a string with a \" randomly in it"
-          -- --     ^count                    ^dont count     ^ count
-          if (T.count "\"" t) > 1 then -- there is more than one quote in the line
-            -- ignore line up through the closing quote, check the rest as regular
-          else -- there is only one unclosed quote on the line
-            -- see else case on False True
-        _ -> undefined -- should never happen
-
-    -- no quotation mark(s) yes line comment
-    (True, False) -> rmPrLnCmt' ((T.breakOn "--" t):acc) ts
-
-    -- TODO yes quotation mark(s) no line comment.
-    (False, True) ->
-
-          -- TODO: change this to only cound non escaped quote
-          -- --  in "this is a string with a \" randomly in it"
-          -- --     ^count                    ^dont count     ^ count
-      if even (T.count "\"" t) then -- all open quotes closed themselves, no comments, treat as regular line
-        rmPrLnCmt' (t:acc) ts
-      else -- if there is an unclosed quotation
-        -- add this line to the acc like any other with no comment
-        -- add all subsequent lines to acc until I find one with a closing quote
-        -- go back to regular loop ignoring up through the closing quote
-
-    -- neither quotation mark(s) nor line comment
-    (False, False) -> rmPrLnCmt' (t:acc) ts
-
-qRmPrLnCmt' :: [T.Text] -> [T.Text] -> [T.Text]
-qRmPrLnCmt' acc [] = reverse acc
-qRmPrLnCmt' acc (t:ts) =
-  -- if t contains no nonescaped " then
-    -- qRmPrLnCmt' (t:acc) ts
-  -- else if there is a nonescaped quote
-    -- if there are no quotes or -- after the closing quote then
-      -- rmPrLnCmt' (t:acc) ts
-    -- else
-      -- if the comment is first then
-        -- rmPrLnCmt' ((T.breakOn "--" t):acc) ts
-      -- else if the quote is first
+emptyTemp :: UCFMLTemplate
+emptyTemp = UCFMLTemplate []
 
 
--- remove block commenta
-rmBlkCmt :: T.Text -> T.Text
-rmBlkCmt rt = undefined
--- look for quotes and {-
--- -- if find a quote skip foward to its close and start look again
--- -- if find a "{-", then find the next "-}" and delete the brackets and any text inbetween
-   -- recurse on the new text without the block comments until end
+-- stations the parser can be at
+data FsmStates = RdTLT   -- reading top layer tags
+               | RdBlCmt -- reading block comment
+               | RdMeta  -- reading meta
+               | RdBody  -- reading body
+               | RdTemp  -- reading template
+               | EOF     -- file ended in a place that's not obviously wrong (inside brackets or quotes etc)
 
--} -- ******* END BIG COMMENT COMMENT BLOCK *********
--- split into Meta, Body, and template --
--- --------------------------------------
--- check that ther are only 3 lines of text touching first two columns, one for each section
--- if so return they're correlated sections, otherwise nothing
-initSplit :: T.Text -> Either T.Text ([T.Text], [T.Text], [T.Text])
-initSplit tx = undefined
 
--- try to parse first chunk into UCFMLMeta --
--- ------------------------------------------
-metagen :: T.Text -> Either T.Text UCFMLMeta
-metagen rt = undefined
+fsm :: [FsmStates] -> UCFMLModel -> (Int, Int) -> T.Text -> UCFMLFile
+-- fsm with an empty list passed to it, shouldn't happen
+fsm [] _ _ _ = ParseError "somehow fsm got an empty [FsmStates]"
 
--- try to parse second chunk into UCFMLBody --
--- -------------------------------------------
-bodygen :: T.Text -> Either T.Text UCFMLBody
-bodygen rt = undefined
+-- fsm at the reading top layer tags station
+fsm sl@(RdTLT:xs) pmod lc@(ln,col) rawtxt =
+  if T.null rawtxt then                      -- if end of file
+    fsm (EOF:sl) pmod lc rawtxt              -- then goto eof station
+  else if (isSpace T.head $ rawtxt) then     -- if it start with white space
+    let (ws,rst) = T.span isSpace rawtxt in  -- then update lc and recurse
+      fsm sl pmod (addWS lc ws) rst
+  else if ("{-" `T.isPrefixOf` rawtxt) then -- open block comment
+    fsm (RdBlCmt:sl) pmod (ln,(col+2))
+  else if ("--" `T.isPrefixOf` rawtxt) then -- handle single line comment this time I already know I'm not in a quote so no worries
+    fsm sl pmod ((ln+1),0) (T.Unlines (tail (T.Lines rawtxt))) -- inc ln, 0 col, pick up again at next line
+  else if () -- checking for tags now
+-- fsm at the EOF station
+fsm sl@(EOF:_) pmod lc _ = verifyPartMod pmod
 
--- try to parse last chunk into UCFMLTemplate --
--- -------------------------------------------
-templategen :: T.Text -> Either T.Text UCFMLTemplate
-templategen rt = undefined
+-- fsm at the RdBlCmt station
+fsm sl@(RdBlCmt:xs) pmod lc@(ln,col) rawtxt =
 
--- one function to run them all
--- -----------------------------
-bigParseFunc :: T.Text -> UCFMLFile
-bigParseFunc = undefined
+
+addWS :: ()
+
+-- make sure that it's a valid UCFMLModel and give an error if it's not
+verifyPartMod :: UCFMLModel -> UCFMLFile
